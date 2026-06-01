9662f05ad5a9d46354f7bf6b275e000d4a77f74b
zinebnadak <zineb@nadak.ai>
2026-06-01 19:14:41 +0300

    feat: add Filereader function

--- src/tools.py
+++ src/tools.py
@@ -1,16 +1,17 @@
 '''
 7 tools in total:
-web_search
-wikipedia
-web_scraper
+calculator
 file_reader
 file_writer
+web_search
+web_scraper
+wikipedia
 code_executor
-calculator
 '''
 
 
 from models import ToolResult
+from pathlib import Path
 import json
 
 def calculator(expression: str) -> ToolResult:      #the user enters an expression, and from this function we will get out predefined model: name, result, success (or error)
@@ -26,9 +27,18 @@ expression_input = str(input("Enter an expression: "))
 print(calculator(expression_input))
 '''
 
-
-
-
+def file_reader(filepath: str) -> ToolResult:
+    try:
+        content = Path(filepath).resolve().read_text()  #normalizes the path to absolute path, then reads
+        return ToolResult(tool_name="file_reader", result=content, success = True, error = None)
+    
+    except Exception as e:
+        return ToolResult(tool_name="file_reader", result=content, success = False, error = str(e))
+        
+'''
+Test:
+print(file_reader("/Users/nadak/ep-003-ai-agent-tools/docs/notes.md"))
+'''
 
 
 '''
@@ -47,6 +57,19 @@ TOOL_SCHEMAS = [
             "required": ["expression"] #arguments that can't be missing when calling the tool goes to required
         }
     }
+
+    {
+        "name" : "file_reader",
+        "description" : "This tool opens a file and reads it",
+        "input_schema" : {
+            "type" : "object",
+            "properties" : {    
+            "filepath" : {"type": "string"} #only one property  
+            },
+            "required": ["filepath"]
+        }
+    }
+
 ]
 
 
@@ -57,7 +80,8 @@ TOOL_SCHEMAS = [
 Dispatch table maps tool names to functions so run_tool() can look them up
 '''
 TOOLS = {
-    "calculator": calculator
+    "calculator" : calculator
+    "file_reader" : file_reader
 }
 
 
@@ -70,7 +94,7 @@ This function is used to run a tool
 '''
 def run_tool(name, inputs) -> ToolResult:
     requested_tool = TOOLS[name]    #look up the tool by name form th dictionary TOOLS
-    return requested_tool(**inputs) #run requested_tool function with the inputs arguments
+    return requested_tool(**inputs) #run requested_tool function with the inputs arguments, **inputs unpacks a dictionary into keyword arguments.
 
 
 
